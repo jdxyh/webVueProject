d78b4ee0f3fa072fd8306862b3c267fdf6a2cdfc
jdxyh <763998827@qq.com>
2018-07-14 20:56:05 +0800

    taishiji

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# webVueProject
+# vuetest
+
+> this is my vuetest
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+# 项目说明文档
+
+# 一：项目实现的需求：
+1 模仿'去哪儿'界面，实现简单的功能浏览操作<br />
+# 二：运行环境搭建
+1 下载 node.js 项目管理工具,使之能运行npm包管理工具<br />
+2 选用git仓库托管平台,github 码云等,操作简单的git命令<br />
+3 下载Vue脚手架工具（详细参看Vue官方说明文档）<br />
+npm install --global vue-cli<br />
+vue init webpack my-project<br />
+cd my-project<br />
+npm install<br />
+npm run dev(npm run start)<br />
+4 查阅Vue目录结构，查看目录结构下各文件的作用<br />
+# 三：
+
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,55 @@
+<template>
+  <!--解决画面抖动-->
+  <div clsass = "wrapper">
+  <swiper :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide>
+      <img class = "sper-img" src = "@/assets/imgs/swiper/1.jpg"/>
+    </swiper-slide>
+    <swiper-slide>
+      <img src = "@/assets/imgs/swiper/2.jpg"/>
+    </swiper-slide>
+    <swiper-slide>
+      <img src = "@/assets/imgs/swiper/3.jpg"/>
+    </swiper-slide>
+    <swiper-slide>
+      <img src = "@/assets/imgs/swiper/4.jpg"/>
+    </swiper-slide>
+    <swiper-slide>
+      <img src = "@/assets/imgs/swiper/5.jpg"/>
+    </swiper-slide>
+    <swiper-slide>
+      <img src = "@/assets/imgs/swiper/6.jpg"/>
+    </swiper-slide>
+    <swiper-slide>
+      <img src = "@/assets/imgs/swiper/7.jpg"/>
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {}
+    }
+  }
+}
+</script>
+
+<style lang = "stylus" scoped>
+  .swiper-img{
+    width:100%
+  }
+  //解决画面抖动方案
+  .wrapper{
+    overflow hidden
+    width:100%
+    height 0
+    padding-bottom 31.25%
+  }
+</style>
