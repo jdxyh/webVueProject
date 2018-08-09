3702d3eab9d4266bf05464939f574952eb60e050
jdxyh <763998827@qq.com>
2018-08-09 22:53:30 +0800

    jdxyh

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
 			<span class="iconfont">&#xe632;</span>
 			输入城市/景点/游玩主题
 		</div>
-		<div class="header-right">
+		<router-link to='/city'>
+			<div class="header-right">
 			{{this.city}}
 			<span class="iconfont">&#xe62d;</span>
-		</div>
+			</div>
+		</router-link>
 	</div>
 </template>
 
@@ -29,7 +31,7 @@ export default {
 	.header
 		display:flex
 		/*1rem = html font-size = 50px*/
-		line-height:0.86rem
+		line-height:$headerHeight
 		background:$bgColor
 		color:#fff
 		.header-left
@@ -52,4 +54,5 @@ export default {
 			width:1.24rem
 			float:right
 			text-align:center
+			color: #fff
 </style>
